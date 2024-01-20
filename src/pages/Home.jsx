9a5ab50024f8ba0333b48f23f606504bedbafb39
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2024-01-20 12:31:21 +0200

    add npm react-three/fiber and folder a

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
+import Island from "../models/Island";
 
 {
 	/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
@@ -9,6 +10,22 @@ import Loader from "../components/Loader";
 }
 
 const Home = () => {
+	const adjustIslandForScreenSize = () => {
+		let screenScale = null;
+		let screenPosition = [0, -6.5, -43];
+		let rotation = [0.1, 4.7, 0];
+
+		if (window.innerWidth < 768) {
+			screenScale = [0.9, 0.9, 0.9];
+		} else {
+			screenScale = [1, 1, 1];
+		}
+
+		return [screenScale, screenPosition, rotation];
+	};
+
+	const [islandScale, islandPosition, islandRotation] =
+		adjustIslandForScreenSize();
 	return (
 		<section className="w-full h-screen relative">
 			<Canvas
@@ -16,11 +33,17 @@ const Home = () => {
 				camera={{ near: 0.1, far: 1000 }}
 			>
 				<Suspense fallback={<Loader />}>
-					<directionalLight />
+					<directionalLight position={[1, 1, 1]} intensity={2} />
 					<ambientLight />
 					<pointLight />
 					<spotLight />
 					<hemisphereLight />
+
+					<Island
+						position={islandPosition}
+						scale={islandScale}
+						rotation={islandRotation}
+					/>
 				</Suspense>
 			</Canvas>
 		</section>
