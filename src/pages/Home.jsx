e136418d06040642a76f0effae4b70a5ce32f072
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2024-01-20 12:55:04 +0200

    add Sky in models

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
 import Island from "../models/Island";
+import Sky from "../models/Sky";
 
 {
 	/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
@@ -34,11 +35,13 @@ const Home = () => {
 			>
 				<Suspense fallback={<Loader />}>
 					<directionalLight position={[1, 1, 1]} intensity={2} />
-					<ambientLight />
-					<pointLight />
-					<spotLight />
-					<hemisphereLight />
-
+					<ambientLight intensity={0.5} />
+					<hemisphereLight
+						skyColor="#b1e1ff"
+						groundColor="#000000"
+						intensity={1}
+					/>
+					<Sky />
 					<Island
 						position={islandPosition}
 						scale={islandScale}
