636a34610453f21c06e0d0dad7e92f8bb280ef81
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2024-01-21 14:15:44 +0200

    create POPUP

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,7 @@ import Island from "../models/Island";
 import Sky from "../models/Sky";
 import Bird from "../models/Bird";
 import Plane from "../models/Plane";
-
-{
-	/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-				POPUP
-			</div> */
-}
+import HomeInfo from "../components/HomeInfo";
 
 const Home = () => {
 	const [isRotating, setIsRotating] = useState(false);
@@ -50,6 +45,10 @@ const Home = () => {
 	const [planeScale, planePosition] = adjustPlaneForScreenSize();
 	return (
 		<section className="w-full h-screen relative">
+			<div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+				{currentStage && <HomeInfo currentStage={currentStage} />}
+			</div>
+
 			<Canvas
 				className={`w-full h-screen bg-transparent" ${
 					isRotating ? "cursor-grabbing" : "cursor-grab"
