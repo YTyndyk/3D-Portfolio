4953ea00b6c15e38dec65d76c606b72025951be7
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2024-01-24 16:42:59 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
 		<header className="header">
 			<NavLink
 				to="/"
-				className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
+				className="w-14 h-10 rounded-lg bg-transparent items-center justify-center flex font-bold shadow-md"
 			>
 				<p className="blue-gradient_text">Home</p>
 			</NavLink>
